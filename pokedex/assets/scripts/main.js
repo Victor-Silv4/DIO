f2ede47f45d3ea998b10a800e5bdf3aeda70e519
Victor Augusto <victor.augustosilva004@gmail.com>
2024-12-01 15:07:19 -0300

    Adicionada a funcionalidade de tela de informações

--- pokedex/assets/scripts/main.js
+++ pokedex/assets/scripts/main.js
@@ -1,5 +1,7 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
+const pokemonInfo = document.getElementById("pokemonInfo");
+
 
 const maxRecords = 151;
 let offset = 0;
@@ -20,6 +22,54 @@ function convertPokemonToHtml(pokemon) {
             </li>`;
 }
 
+function convertPokemonToOverlay(pokemon) {
+    return `<div class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.icon}" alt="${pokemon.name}">
+                </div>
+            </div>
+            
+          
+            
+            <div class="additionalInfos">
+                    <div class="abilitiesInfo">
+                        <span>Abilities</span>
+                        <ol>
+                            <li class="${pokemon.type}">${pokemon.abilities[0]}</li>
+                            <li class="${pokemon.type}">${pokemon.abilities[1]}</li>
+                        </ol>
+                    </div>
+                    
+                    
+                    
+                <div class="moveInfo">
+                    <span>Moves</span>
+                    <div class="movePositions">
+                        <ol class="moves">
+                            
+                        </ol>
+                    </div>
+                </div>
+            </div>`;
+ }
+
+ function loadMoveItems(pokemon, newMovesCount) {
+    // Verifica se o Pokémon já tem movimentos carregados
+    const moveList = pokemon.moves; // Pega os novos movimentos
+    const moveTypes = pokemon.moveTypes; // Pega os tipos dos movimentos
+
+    const moveItemsHtml = moveList.map((move, index) => {
+        return `<li class="${moveTypes[index]}">${move}</li>`;
+    }).join('');
+
+    const moveContainer = document.querySelector('.movePositions .moves');
+    moveContainer.innerHTML += moveItemsHtml;
+}
 
 
 function loadPokemonItems(offset, limit) {
@@ -60,4 +110,42 @@ loadMoreButton.addEventListener("click", () => {
 
 })
 
+pokemonList.addEventListener('click', (event) => {
+    const pokemonElement = event.target.closest('.pokemon');
+    const pokemonNumberElement = pokemonElement.querySelector('.number');
+        
+    const pokemonNumber = pokemonNumberElement ? pokemonNumberElement.innerText.replace('#', '') : null;
+
+    const overlayContent = document.querySelector('.overlay-content');
+    const overlay = document.querySelector('.overlay');
+
+
+    if (pokemonNumber) {
+        pokeApi.getPokemonByOffset(pokemonNumber)
+            .then((pokemon) => {
+                console.log(pokemon);  // Exibe o Pokémon
+                overlayContent.innerHTML = convertPokemonToOverlay(pokemon);
+                overlay.style.display = "flex";
+                loadMoveItems(pokemon, 4);
+
+                let newMoves = 4;
+                let movesLimit = 4;
+
+                overlay.addEventListener('scroll', () => {
+                    if(movesLimit <= 8) {
+                        movesLimit += 4;
+                        loadMoveItems(pokemon, newMoves);
+                    }
+                    
+                });
+
+                overlay.addEventListener('click', () => {
+                    overlay.style.display = "none";
+                });
+            })
+            
+            .catch((error) => console.log(error));
+    }
+
+});
 
--- pokedex/assets/scripts/pokeApi.js
+++ pokedex/assets/scripts/pokeApi.js
@@ -1,15 +1,26 @@
 
 const pokeApi = {};
 
-function convertPokeApiDetailToPokemon(pokemonDetail) {
+async function convertPokeApiDetailToPokemon(pokemonDetail) {
     const pokemon = new Pokemon();
     pokemon.name = pokemonDetail.name;
     pokemon.number = pokemonDetail.id;
-    pokemon.types = pokemonDetail.types.map(typeSlot => typeSlot.type.name);
+    pokemon.types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name);
     pokemon.type = pokemonDetail.types[0].type.name;
     pokemon.icon = pokemonDetail.sprites.other.dream_world.front_default;
-    
-    console.log(pokemon.number)
+    pokemon.moves = pokemonDetail.moves.slice(0, 40).map((moveSlot) => moveSlot.move.name);
+    pokemon.abilities = pokemonDetail.abilities.map((abilitieSlot) => abilitieSlot.ability.name);
+
+    // Fazer requisições para obter os detalhes dos movimentos
+    const moveDetailsPromises = pokemonDetail.moves.slice(0, 40).map(async (moveSlot) => {
+        const response = await fetch(moveSlot.move.url);
+        const moveDetail = await response.json();
+        return moveDetail.type.name; // Retorna o tipo do movimento
+    });
+
+    // Resolver todas as promessas e adicionar os tipos ao Pokémon
+    pokemon.moveTypes = await Promise.all(moveDetailsPromises);
+
     return pokemon;
 }
 
@@ -27,5 +38,13 @@ pokeApi.getPokemons = (offset = 0, limit = 9) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequest) => Promise.all(detailRequest))
         .then((pokemonDetail) => pokemonDetail)
+        .catch((error) => console.log(error));
+}
+
+pokeApi.getPokemonByOffset = (offset) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${offset}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
         .catch((error) => console.log(error));
 }
